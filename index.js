47a12645e34c979df2371ac489a08b35cb6ea08f
Prashnil Prasad <prashnilprasad@Prashnils-MacBook-Pro.local>
2020-04-26 13:33:49 -0400

    css file

---
+++ index.js
@@ -0,0 +1,23 @@
+const jokeElement = document.getElementById('joke');
+const get_joke = document.getElementById('get_joke');
+
+get_joke.addEventListener('click', generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  // call the icanhaz API
+  const jokeResult = await fetch ('https://icanhazdadjoke.com/', {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  
+  const joke = await jokeResult.json();
+  
+  console.log(joke);
+  
+  // set the new joke
+  jokeElement.innerHTML = joke.joke;
+  
+}
